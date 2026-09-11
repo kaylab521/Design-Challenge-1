60021fb83d39d3894afba1f3e95655f2640e88f0
kaylab521 <Michaela.b521@gmail.com>
2026-09-10 22:42:00 -0700

    toggle themes

---
+++ script.js
@@ -0,0 +1,44 @@
+const spicyButton = document.getElementById("spicyButton");
+
+  spicyButton.addEventListener("click", () => {
+    document.body.classList.toggle("spicy-theme");
+  }); 
+
+const happyButton = document.getElementById("happyButton");
+
+  happyButton.addEventListener("click", () => {
+    document.body.classList.toggle("happy-theme");
+  });  
+const sadButton = document.getElementById("sadButton");
+
+  sadButton.addEventListener("click", () => {
+    document.body.classList.toggle("sad-theme");
+  });
+const spaceButton = document.getElementById("spaceButton");
+
+  spaceButton.addEventListener("click", () => {
+    document.body.classList.toggle("space-theme");
+  });
+const chillButton = document.getElementById("chillButton");
+
+  chillButton.addEventListener("click", () => {
+    document.body.classList.toggle("chill-theme");
+  });
+const euphoriaButton = document.getElementById("euphoriaButton");
+
+  euphoriaButton.addEventListener("click", () => {
+    document.body.classList.toggle("euphoria-theme");
+  });
+const romanticButton = document.getElementById("romanticButton");
+
+  romanticButton.addEventListener("click", () => {
+    document.body.classList.toggle("romantic-theme");
+  });
+const luckyButton = document.getElementById("luckyButton");
+
+  luckyButton.addEventListener("click", () => {
+    document.body.classList.toggle("lucky-theme");
+  });
+
+
+   
